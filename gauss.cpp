0d05cd855d2fee64a00b1d22150f28490f8eb0c3
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-10-31 18:44:11 +0000

    Correccion de errores en el metodo

--- gauss.cpp
+++ gauss.cpp
@@ -6,10 +6,13 @@ int main(){
 	float pi = 2.0*asin(1.0);
 	srand48(8);
 	float p = 0.5;
+	float r;
+	float alpha;
+	float pr;
 	for(int i=0; i<10000; i++){
-        float pr = p + (drand48()-0.5);
-        float r = (exp(-p*p)/sqrt(2.0*pi))/(exp(-pr*pr)/sqrt(2.0*pi));
-        float alpha = drand48();
+        pr = p + (drand48()-0.5);
+        r = (exp((-pr)*pr)/sqrt(2.0*pi))/(exp((-p)*p)/sqrt(2.0*pi));
+        alpha = drand48();
         if(alpha<r){
             p=pr;
 		}
--- gauss.py
+++ gauss.py
@@ -5,5 +5,5 @@ import matplotlib.pyplot as plt
 data = np.loadtxt("gauss.dat")
 
 plt.figure()
-plt.hist(data, bins=30, density=True, label='Metropolis-Hastings')
+plt.hist(data, bins=20, density=True, label='Metropolis-Hastings')
 plt.savefig("gauss.png")
